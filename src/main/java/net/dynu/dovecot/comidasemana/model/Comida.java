7f38be5b3d6bf111c398f4a6fd456013ac237f1a
EnriqueRS <enrique.rios.santos@gmail.com>
2020-06-15 08:10:48 +0200

    Initial commit

---
+++ src/main/java/net/dynu/dovecot/comidasemana/model/Comida.java
@@ -0,0 +1,40 @@
+package net.dynu.dovecot.comidasemana.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Comida {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+	private String name;
+	private String image;
+	private String url;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+}
